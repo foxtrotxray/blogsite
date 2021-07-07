88a4eaa2e35b1d8ec488f37820115da592b63048
Kyle Collins <kyleskycollins@gmail.com>
2021-07-06 17:19:22 -0700

    fix another PORT typo

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
-  console.log(`Blogsite online @ http://localhost:${port}`)
+  console.log(`Blogsite online @ http://localhost:${PORT}`)
 })
