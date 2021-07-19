e6906b6b00740c9341dd9931b486b546a3afb6e2
Kyle Collins <kyleskycollins@gmail.com>
2021-07-19 11:57:19 -0700

    runs express with some placeholder routes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
+
+// get ten posts from the DB 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Placeholder for the first ten blog posts!')
 })
+
+// if there are ten more posts, get them; or all remaining
+app.get('/more', (req, res) => {
+    
+    res.send('sending more entries!')
+})
+
 
 exports.server = app;
