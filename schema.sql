b6465c408fd51a984d1620fa347a3db25ba97344
Kyle Collins <kyleskycollins@gmail.com>
2021-07-22 13:47:25 -0700

    adds postgres and the first iteration of the schema

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE "public.authors" (
+	"author_id" serial NOT NULL,
+	"author_name" character varying(100) NOT NULL,
+	"author_password" TEXT NOT NULL,
+	CONSTRAINT "authors_pk" PRIMARY KEY ("author_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.articles" (
+	"article_id" serial NOT NULL,
+	"author_id" int NOT NULL,
+	"published" timestamp with time zone NOT NULL,
+	"title" character varying(510) NOT NULL UNIQUE,
+	"content" TEXT NOT NULL,
+	CONSTRAINT "articles_pk" PRIMARY KEY ("article_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "articles" ADD CONSTRAINT "articles_fk0" FOREIGN KEY ("author_id") REFERENCES "authors"("author_id");
+
+
+
