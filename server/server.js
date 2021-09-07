d1910ef2abe8fb19079bf3633e6a6e4787554c22
Kyle Collins <kyleskycollins@gmail.com>
2021-09-07 11:16:26 -0700

    adds a title form, checks username & password for article submissions

--- server/server.js
+++ server/server.js
@@ -16,16 +16,30 @@ function buildServer(db) {
         res.send('sending more entries!')
     })
 
+
     app.post("/submit", async (req, res) => {
         console.log("Request body:", req.body)
+        let credentials = [req.body.name, req.body.password];
+        let authorTest  = await db.query(
+            `SELECT author_id FROM authors
+            WHERE author_name = '${credentials[0]}' 
+            AND author_password = '${credentials[1]}'`);
+        if (authorTest.rowCount === 0) {
+            res.send("Incorrect name or password!");
+        } else {
+            console.log("AuthorID: " + authorTest.rows[0].author_id);
+            // db.query(`INSERT INTO articles VALUES(
+            //     default, 
+            //     ${authorTest.rows[0].author_id},
+            //     now(), 'very technical article, part 2', 'whereas UDP is equally challenging...');`)
+            // let results = await db.query("SELECT * FROM articles");
+            // results.rows.forEach(row => {
+            //     console.log(row)
+            // })
+    
+            res.send("received");
 
-        let results = await db.query("SELECT * FROM articles");
-
-        results.rows.forEach(row => {
-            console.log(row)
-        })
-
-        res.send("received")
+        }
     })
 
     return app;
