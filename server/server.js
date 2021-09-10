f7000f67993c3d73c6fc396207c5e4dd1688598e
Kyle Collins <kyleskycollins@gmail.com>
2021-09-10 14:41:51 -0700

    includes db path in the npm start command; root route displays a list of articles

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const app = express()
 const submissionText = "INSERT INTO articles(author_id, published, title, content) VALUES ($1, $2, $3, $4)";
-
+const authText = "SELECT author_id FROM authors WHERE author_name = $1 AND author_password = $2";
 
 function buildServer(db) {
     app.use(express.static('public'))
     app.use(express.urlencoded({ extended: true }));
     
-    // client.conn 
-    // get ten posts from the DB 
-    app.get('/', (req, res) => {
-        res.send('Placeholder for the first ten blog posts!')
+    // Get & display the titles of all articles
+    app.get('/', async (req, res) => {
+        let htmlBuilder = "<ol>"  
+        let results = await db.query("SELECT title, article_id FROM articles ");
+        results.rows.forEach(row => {
+            console.log(row);
+            htmlBuilder += `<li><a href="view/${row.article_id}">${row.title}</a></li>`;
+        })
+        htmlBuilder += "</ol>";
+        
+        res.send(htmlBuilder);
+
+
+
     })
     
     // if there are ten more posts, get them; or all remaining
@@ -22,10 +32,7 @@ function buildServer(db) {
     app.post("/submit", async (req, res) => {
         console.log("Request body:", req.body)
         let credentials = [req.body.name, req.body.password];
-        let authorTest  = await db.query(
-            `SELECT author_id FROM authors
-            WHERE author_name = '${credentials[0]}' 
-            AND author_password = '${credentials[1]}'`);
+        let authorTest  = await db.query(authText, credentials);
         if (authorTest.rowCount === 0) {
             res.send("Incorrect name or password!");
         } else {
@@ -38,10 +45,11 @@ function buildServer(db) {
             } catch (err) {
                 console.log(err.stack)
             }
-            let results = await db.query("SELECT * FROM articles");
-            results.rows.forEach(row => {
-                console.log(row)
-            })
+            // this snippet displays all the articles in the console
+            // let results = await db.query("SELECT * FROM articles");
+            // results.rows.forEach(row => {
+            //     console.log(row)
+            // })
     
             res.send("received");
 
